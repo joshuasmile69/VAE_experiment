13645ec7bfba7aadbf7b6fb54fbe45ce63660aaa
joshuasmile69 <joshuasmile69@gmail.com>
2024-06-02 18:03:04 +0900

    Update loss.py

--- loss.py
+++ loss.py
@@ -36,9 +36,66 @@ class LogManager:
             print(stat_type,":",stat, end=' / ')
         print(" ")
 
-    
+    def get_stat_summary(self):
+        summary = ""
+        for stat_type, values in self.log_book.items():
+            stat = self.get_stat(stat_type)
+            summary += f"{stat_type}: {stat}\n"
+        return summary
+        '''
 
+import torch
+import numpy as np
 
+class LogManager:
+    def __init__(self):
+        self.log_book = dict()
+
+    def alloc_stat_type(self, stat_type):
+        self.log_book[stat_type] = []
+
+    def alloc_stat_type_list(self, stat_type_list):
+        for stat_type in stat_type_list:
+            self.alloc_stat_type(stat_type)
+
+    def init_stat(self):
+        for stat_type in self.log_book.keys():
+            self.log_book[stat_type] = []
+
+    def add_stat(self, stat_type, stat):
+        assert stat_type in self.log_book, "Wrong stat type"
+        self.log_book[stat_type].append(stat)
+
+    def add_torch_stat(self, stat_type, stat):
+        assert stat_type in self.log_book, "Wrong stat type"
+        self.log_book[stat_type].append(stat.detach().cpu().item())
+
+    def get_stat(self, stat_type):
+        result_stat = 0
+        stat_list = self.log_book[stat_type]
+        if len(stat_list) != 0:
+            result_stat = np.mean(stat_list)
+            result_stat = np.round(result_stat, 4)
+        return result_stat
+
+    def print_stat(self):
+        for stat_type in self.log_book.keys():
+            stat = self.get_stat(stat_type)
+            if stat != 0:
+                print(stat_type, ":", stat, end=' / ')
+        print(" ")
+
+    def get_stat_summary(self):
+        summary = ""
+        for stat_type, values in self.log_book.items():
+            stat = self.get_stat(stat_type)
+            summary += f"{stat_type}: {stat}\n"
+        return summary
+
+    def save_to_file(self, file_path):
+        with open(file_path, 'w') as f:
+            f.write(self.get_stat_summary())
+            
 def l1loss(pred, true):
     return F.l1_loss(pred, true)
 
