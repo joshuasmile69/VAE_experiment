19ca5685f3958234a8df230de8c258c6fe18992b
doldol <117711062+doldol330@users.noreply.github.com>
2024-06-02 12:38:46 +0900

    Update print_graph.py

--- print_graph.py
+++ print_graph.py
@@ -1,4 +1,7 @@
 import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--log_path', type=str)
 parser.add_argument('--model_path', type=str)
@@ -8,37 +11,33 @@ args = parser.parse_args()
 log_path = args.log_path
 model_path = args.model_path
 out_path = args.out_path
+
 train_loss_list = []
 dev_loss_list = []
 
 with open(log_path, 'r') as f:
     for line in f:
-        line = line[:-1]
-        if line != model_path:
-            continue
+        epoch, mtype, total_loss = line.strip().split(',')
+        total_loss = float(total_loss)
         
-        for line in f:
-            if line[0] == "*":
-                break
-            info = line.split(": ")
-            if info[0] not in ["Train", "DEV"]:
-                continue
-            mtype = info[0]
-            total_loss = float(info[-1].split(" / ")[0])
-            
-            if mtype == "Train":
-                train_loss_list.append(total_loss)
-            if mtype == "DEV":
-                dev_loss_list.append(total_loss)
-        break
+        if mtype == "Train":
+            train_loss_list.append(total_loss)
+        elif mtype == "DEV":
+            dev_loss_list.append(total_loss)
 
 with open(out_path, 'w') as f:
     f.write("epoch,Train,Dev\n")
     for epoch in range(len(train_loss_list)):
-        f.write(str(epoch)+",")
-        f.write(str(train_loss_list[epoch])+",")
-        f.write(str(dev_loss_list[epoch])+",")
-        f.write('\n')
+        f.write(f"{epoch},{train_loss_list[epoch]},{dev_loss_list[epoch]}\n")
 
-
-
+# 그래프 그리기
+epochs = range(len(train_loss_list))
+plt.figure(figsize=(10, 5))
+plt.plot(epochs, train_loss_list, label='Train Total Loss')
+plt.plot(epochs, dev_loss_list, label='Dev Total Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Total Loss')
+plt.title('Training and Development Total Loss')
+plt.legend()
+plt.savefig(out_path.replace('.csv', '.png'))
+plt.show()
